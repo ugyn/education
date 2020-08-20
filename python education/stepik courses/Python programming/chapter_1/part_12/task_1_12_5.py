5eb0f021af9f386516414e0467eea431d88af3f4
ugyn <gynexer@gmail.com>
2020-08-20 19:49:00 +0300

    added task_1_12_5

--- python education/stepik courses/Python programming/chapter_1/part_12/task_1_12_5.py
+++ python education/stepik courses/Python programming/chapter_1/part_12/task_1_12_5.py
@@ -1,1 +1,39 @@
-keks
+n1, n2, n3 = int(input()), int(input()), int(input())
+if n1 >= n2:
+    if n2 >= n3:
+        maximal = n1
+        average = n2
+        minimal = n3
+    elif n1 >= n3:
+        maximal = n1
+        average = n3
+        minimal = n2
+    else:
+        maximal = n3
+        average = n1
+        minimal = n2
+elif n2 >= n3:
+    maximal = n2
+    if n3 >= n1:
+        average = n3
+        minimal = n1
+    else:
+        average = n1
+        minimal = n3
+else:
+    maximal = n3
+    average = n2
+    minimal = n1
+print(maximal, minimal, average)
+
+'''
+a,b,c = int(input()), int(input()), int(input())
+
+if a < b:
+	a, b = b, a
+if a < c:
+	a, c = c, a
+if b > c:
+	b, c = c, b
+print(a, b, c, sep='\n')
+'''
